e8214df248da55b798b738f44e3f33cf9c343d72
MetaDogeUnity <144910624+metadogeunity@users.noreply.github.com>
2026-09-14 18:52:42 +0530

    Flatten Vite app to repository root

--- ai-organization/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AIOrganization from "./AIOrganization.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AIOrganization />
-  </React.StrictMode>
-);
